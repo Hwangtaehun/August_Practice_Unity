fe60381364c80ee182785d336aa68e20f1179c46
Hwangtaehun <r283@naver.com>
2022-08-02 21:47:36 +0900

    스크립트 작성
    ....

--- Dodge/Assets/Script/PlayerController.cs
+++ Dodge/Assets/Script/PlayerController.cs
@@ -4,18 +4,52 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody;
+    private Rigidbody playerRigidbody;
     public float speed = 8.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            playerRigidbody.AddForce(0.0f, 0.0f, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            playerRigidbody.AddForce(0.0f, 0.0f, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            playerRigidbody.AddForce(speed, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            playerRigidbody.AddForce(-speed, 0.0f, 0.0f);
+        }
+
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Verticla");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        playerRigidbody.velocity = newVelocity;
     }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+
 }
+
